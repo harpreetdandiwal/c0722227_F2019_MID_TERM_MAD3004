128599c722a95d033911dd63e5db8cee1eb9fdbc
harpreet <dandiwal357@gmail.com>
2019-10-19 18:47:04 -0400

    init

---
+++ c0722227_F2019_MID_TERM_MAD3004/mobile.swift
@@ -0,0 +1,22 @@
+//
+//  mobile.swift
+//  c0722227_F2019_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2019-10-19.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+class mobile: bill
+{
+    var manufacturer:String?
+    var plan:Int?
+    var mobilenumber:Int?
+    var internetusage:Int?
+    var minutesusage:Int?
+     init(billId: Int, billdate: Date, totalamount: Float, types: billtypes,manufacturer:String,plan:Int,mobilenumber:Int,internetusage:Int,minuteusage:Int)
+     {
+        <#code#>
+    }
+    
+}
