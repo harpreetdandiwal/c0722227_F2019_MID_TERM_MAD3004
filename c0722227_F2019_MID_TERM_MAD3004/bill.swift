362552fc3e6f80d3fa9d8a5d114047125ecb32a9
harpreet <dandiwal357@gmail.com>
2019-10-19 17:48:47 -0400

    class bill

---
+++ c0722227_F2019_MID_TERM_MAD3004/bill.swift
@@ -0,0 +1,20 @@
+//
+//  bill.swift
+//  c0722227_F2019_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2019-10-19.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+class bill:IDisplay
+{
+    var billId:Int?
+    var billdate = Date()
+    var totalamount:float?
+    var types:
+    enum billtypes
+    {
+        
+    }
+}
